eae9dc493a1c6eb7e3a658b34799ab75d54a0e06
Marcus <Marcus@10.21.20.98>
2016-10-05 15:02:00 -0500

    updated RSA algorithm

--- TCP-IP_Sym_Encrypt/src/Sym_Decrypt/Sym_Decrypt.java
+++ TCP-IP_Sym_Encrypt/src/Sym_Decrypt/Sym_Decrypt.java
@@ -1,19 +1,42 @@
 package Sym_Decrypt;
 
 import java.util.Random;
+import org.apache.commons.math3.primes.Primes;
+
 
 public class Sym_Decrypt {
 	private byte [] MyPublicKey = null;
+	private long decryptKey = 11;
+	private long n = 0;
 	
 	public byte [] GetPublicKey () {
 		return MyPublicKey;
 	}
 	
-	private Random rand = new Random();
-	private long prime1 = 0;
-	private long prime2 = 0;
-	private long n = 0;
-	private long z = 0;
-	
+	public Sym_Decrypt() {
+		Random rand = new Random();
+		long prime1 = 0;
+		long prime2 = 0;
+		long z = 0;
+		long encryptKey = 4;
+		
+		prime1 = Primes.nextPrime( rand.nextInt( 5000 ) + 1000 ); // Returns a random prime greater than 1000
+		prime2 = Primes.nextPrime( rand.nextInt( 1000 ) + 8000 ); // Returns a random prime greater than 8000
+		
+		n = prime1 * prime2;
+		z = (prime1 - 1) * (prime2 - 1);
+		
+		// Finds a prime for the encrypt key that is 100 < encryptKey < z
+		while ( !Primes.isPrime( (int) encryptKey ) && (encryptKey < z) && (encryptKey % z != 0) ) {
+			encryptKey = Primes.nextPrime( 100 + rand.nextInt( (int) z ) );
+		}
+		
+		while ( !( (encryptKey * decryptKey) % z == 1) )
+			decryptKey = Primes.nextPrime( (int) decryptKey + 1 );
+		
+		
+		decryptKey = Primes.nextPrime( rand.nextInt( 1000 ) + 100 ); // Returns random prime greater than 100
+		
+	}
 	
 }
