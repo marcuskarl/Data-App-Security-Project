3224bd9428b009b8f8e3c863e53149625ee0324d
Marcus <Marcus@10.21.20.98>
2016-10-05 17:46:06 -0500

    Keys are created in decent time now

---
+++ TCP-IP_Sym_Encrypt/src/Client_Test/Client_Test.java
@@ -0,0 +1,12 @@
+package Client_Test;
+
+import Sym_Decrypt.Sym_Decrypt;
+
+public class Client_Test {
+
+	public static void main(String[] args) {
+
+		Sym_Decrypt x = new Sym_Decrypt();
+	}
+
+}
--- TCP-IP_Sym_Encrypt/src/Sym_Decrypt/Sym_Decrypt.java
+++ TCP-IP_Sym_Encrypt/src/Sym_Decrypt/Sym_Decrypt.java
@@ -6,7 +6,8 @@ import org.apache.commons.math3.primes.Primes;
 
 public class Sym_Decrypt {
 	private byte [] MyPublicKey = null;
-	private long decryptKey = 0;
+	private long decryptKey = 3;
+	private long encryptKey = 101;
 	private long n = 0;
 	
 	public byte [] GetPublicKey () {
@@ -18,7 +19,6 @@ public class Sym_Decrypt {
 		long prime1 = 0;
 		long prime2 = 0;
 		long z = 0;
-		long encryptKey = 4;
 		
 		prime1 = Primes.nextPrime( rand.nextInt( 5000 ) + 1000 ); // Returns a random prime greater than 1000
 		prime2 = Primes.nextPrime( rand.nextInt( 1000 ) + 8000 ); // Returns a random prime greater than 8000
@@ -26,16 +26,33 @@ public class Sym_Decrypt {
 		n = prime1 * prime2;
 		z = (prime1 - 1) * (prime2 - 1);
 		
+		findKeys(z, rand);
+		
+		System.out.println(prime1);
+		System.out.println(prime2);
+		System.out.println(n);
+		System.out.println(z);
+		System.out.println(decryptKey);
+		System.out.println(encryptKey);
+	}
+	
+	private void findKeys (long z, Random rand) {
 		// Finds a prime for the encrypt key that is 100 < encryptKey < z
 		do {
-			encryptKey = Primes.nextPrime( 100 + rand.nextInt( (int) z ) );
-		} while ( (encryptKey < z) && (encryptKey % z != 0) );
+			encryptKey = Primes.nextPrime( 100 + rand.nextInt( (int)Math.sqrt( z ) ) );
+		} while ( encryptKey % z == 0 );
 		
-		while ( !( (encryptKey * decryptKey) % z == 1) )
+		decryptKey = 101;
+		
+		while ( !( (encryptKey * decryptKey) % z == 1) ) {
+			//System.out.println(decryptKey);
 			decryptKey = Primes.nextPrime( (int) decryptKey + 1 );
-		
-		
-		decryptKey = Primes.nextPrime( rand.nextInt( 1000 ) + 100 ); // Returns random prime greater than 100
+			
+			// If decryptKey prime value is greater than z, 
+			// a new value will be found for the encryption key and the process starts over
+			if (decryptKey > z)
+				findKeys(z, rand);
+		}
 		
 	}
 	
